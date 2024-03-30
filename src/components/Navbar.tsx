bcaf46abc1bfe54fe9635512fed0d926b0d0babe
Riddhi-chavan <riddhic164@gmail.com>
2024-03-31 05:19:58 +0530

    making navbar dynamic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const  Navbar = async () => {
                                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                                     {user ? null : <Link href='/sign-in' className={buttonVariants({ variant: "ghost", })}> Sign in </Link>}
                                     {user ? null : <span className="h-6 w-px bg-gray-200" aria-hidden="true" />}
-                                    {user ? <UserAccountNav/> : <Link href="/sign-up" className={buttonVariants({ variant: "ghost", })}>
+                                    {user ? <UserAccountNav user={user}/> : <Link href="/sign-up" className={buttonVariants({ variant: "ghost", })}>
                                         Create account
                                     </Link>}
                                     {user ? (
