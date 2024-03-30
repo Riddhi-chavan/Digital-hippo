d39c08fd5444686c17170103d2253d8784cc8df3
Riddhi-chavan <riddhic164@gmail.com>
2024-03-31 04:27:17 +0530

    first commit

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import { appRouter } from '@/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+
+const handler = async (req: Request): Promise<Response> => {
+    return fetchRequestHandler({
+        endpoint: "/api/trpc",
+        req,
+        router: appRouter,
+        //@ts-expect-error context passed from express middleware
+        createContext: () => ({}),
+    });
+}
+
+export { handler as GET, handler as POST }
